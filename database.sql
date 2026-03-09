c6bdb6d49074e1462de15f600a50d8b3741844d8
Estephanie Anne De Torres <140788832+estephaniedetorres@users.noreply.github.com>
2026-03-09 10:18:30 +0800

    Add files via upload

--- database.sql
+++ database.sql
@@ -26,9 +26,16 @@ CREATE TABLE IF NOT EXISTS `answers` (
   `question_id` int DEFAULT NULL,
   `answer` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table survey_form.answers: ~0 rows (approximately)
+REPLACE INTO `answers` (`id`, `response_id`, `question_id`, `answer`) VALUES
+	(7, 3, 4, 'Oo'),
+	(8, 3, 5, 'Lapis'),
+	(9, 3, 6, 'Matcha'),
+	(10, 4, 4, 'Hindi'),
+	(11, 4, 5, 'Papel'),
+	(12, 4, 6, 'Pistachio');
 
 -- Dumping structure for table survey_form.options
 CREATE TABLE IF NOT EXISTS `options` (
@@ -36,9 +43,14 @@ CREATE TABLE IF NOT EXISTS `options` (
   `question_id` int DEFAULT NULL,
   `option_text` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table survey_form.options: ~0 rows (approximately)
+REPLACE INTO `options` (`id`, `question_id`, `option_text`) VALUES
+	(9, 5, 'Papel'),
+	(10, 5, 'Lapis'),
+	(11, 6, 'Matcha'),
+	(12, 6, 'Pistachio');
 
 -- Dumping structure for table survey_form.questions
 CREATE TABLE IF NOT EXISTS `questions` (
@@ -46,10 +58,15 @@ CREATE TABLE IF NOT EXISTS `questions` (
   `survey_id` int DEFAULT NULL,
   `question` text,
   `type` varchar(50) DEFAULT NULL,
+  `required` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table survey_form.questions: ~0 rows (approximately)
+REPLACE INTO `questions` (`id`, `survey_id`, `question`, `type`, `required`) VALUES
+	(4, 2, 'Masaya ka ba?', 'text', 1),
+	(5, 2, 'Magbigay ng bagay na ginagamit sa eskwelahan', 'radio', 1),
+	(6, 2, 'Magbigay ng pagkain na kulay berde', 'checkbox', 1);
 
 -- Dumping structure for table survey_form.responses
 CREATE TABLE IF NOT EXISTS `responses` (
@@ -57,9 +74,12 @@ CREATE TABLE IF NOT EXISTS `responses` (
   `survey_id` int DEFAULT NULL,
   `submitted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table survey_form.responses: ~0 rows (approximately)
+REPLACE INTO `responses` (`id`, `survey_id`, `submitted_at`) VALUES
+	(3, 2, '2026-03-08 09:58:36'),
+	(4, 2, '2026-03-08 09:58:50');
 
 -- Dumping structure for table survey_form.surveys
 CREATE TABLE IF NOT EXISTS `surveys` (
@@ -70,9 +90,11 @@ CREATE TABLE IF NOT EXISTS `surveys` (
   `password` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table survey_form.surveys: ~0 rows (approximately)
+REPLACE INTO `surveys` (`id`, `title`, `description`, `token`, `password`, `created_at`) VALUES
+	(2, 'FAMILY FEUD', 'entertainment', '0d32965467', '$2y$10$Qa8kkUSAtMckJ/KSgbQSQ.Jer9yy2X1oJ4CGd/hZ3cySo3YgE7FV.', '2026-03-08 09:57:12');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
